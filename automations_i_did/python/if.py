a6823baf32fef2d86639e614bf4516c51f1659dc
Gokularaman Ramamoorthy <Gokularaman_Ramamoorthy@bmc.com>
2025-03-16 22:52:26 +0530

    ifelse

--- automations_i_did/python/if.py
+++ automations_i_did/python/if.py
@@ -4,11 +4,15 @@ if name == "baby":
 
 
 name = input()
+age = int(input())
 # name = "gocool"
 if name == "gocool":
     print(name + " is a good name")
+    print(age + " is your age")
+elif age > 18:
+    print("atleast you are major" )
 else:
-    print(name + " is a bad name")
+    print(name + " is a bad name and " + age + " bad age")
 
 print("*"*40)
 print("Program Completed")
