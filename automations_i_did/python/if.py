072952b4a2feae9812a7fb2824ed641cf3aef2dc
Gokularaman Ramamoorthy <Gokularaman_Ramamoorthy@bmc.com>
2025-03-16 22:44:24 +0530

    ifelse

---
+++ automations_i_did/python/if.py
@@ -0,0 +1,15 @@
+name="baby"
+if name == "baby":
+    print ("Hi " + name)
+
+
+
+name = "gocool"
+if name == "gocool":
+    print(name + " is a good name")
+else:
+    print("bad name")
+
+print("*"*40)
+print("Program Completed")
+print("*"*40)
