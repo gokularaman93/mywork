73f356bb9210802c7b48a20053895e5c7e5a9fa0
Gokularaman Ramamoorthy <Gokularaman_Ramamoorthy@bmc.com>
2025-03-17 14:31:14 +0530

    for.py

---
+++ learnCoding/python/for.py
@@ -0,0 +1,16 @@
+for i in range(1, 5):
+    if i == 3:
+        continue
+    print("A for Apple" + str(i))
+
+total = 0
+for num in range(101):
+    total = total + num
+print(total)
+
+for i in range(0,10,2):
+    print("Even numbers: " + str(i))
+
+
+for i in range(5,-1,-1):
+    print("Even numbers: " + str(i))
